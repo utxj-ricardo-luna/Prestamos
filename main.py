84f057043b8e350fd1b9dabf160f1d92e9975683
M.T.I. Ricardo Luna Santos <143124590+utxj-ricardo-luna@users.noreply.github.com>
2025-03-22 09:12:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routes.users import user
 from routes.material import material
@@ -7,7 +8,14 @@ app = FastAPI(
     title="PRESTAMOS S.A. de C.V.",
     description="API de prueba para almacenar registros de prestamo de material educativo."
 )
-
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 app.include_router(user)
 app.include_router(material)
 app.include_router(prestamo)
